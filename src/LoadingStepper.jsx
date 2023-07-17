982b99e84c1e781906a963047b11b8c88e235382
vivek sharma <vivek26jan2000@gmail.com>
2023-07-17 14:56:01 +0530

    inital commit

---
+++ src/LoadingStepper.jsx
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepButton from "@mui/material/StepButton";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+const steps = [
+  "Select campaign settings",
+  "Create an ad group",
+  "Create an ad",
+];
+
+export default function LoadingStepper() {
+  const [activeStep, setActiveStep] = useState(0);
+  const [completed, setCompleted] = useState({});
+
+  const totalSteps = () => {
+    return steps.length;
+  };
+
+  const completedSteps = () => {
+    return Object.keys(completed).length;
+  };
+
+  const isLastStep = () => {
+    return activeStep === totalSteps() - 1;
+  };
+
+  const allStepsCompleted = () => {
+    return completedSteps() === totalSteps();
+  };
+
+  const handleStep = (step) => () => {
+    setActiveStep(step);
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (!completed[activeStep]) {
+        handleComplete();
+      }
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [activeStep, completed]);
+
+  const handleComplete = () => {
+    const newCompleted = { ...completed };
+    newCompleted[activeStep] = true;
+    setCompleted(newCompleted);
+    handleNext();
+  };
+
+  const handleNext = () => {
+    const newActiveStep =
+      isLastStep() && !allStepsCompleted()
+        ? steps.findIndex((step, i) => !(i in completed))
+        : activeStep + 1;
+    setActiveStep(newActiveStep);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+    setCompleted({});
+  };
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Stepper nonLinear activeStep={activeStep} orientation="vertical">
+        {steps.map((label, index) => (
+          <Step key={label} completed={completed[index]}>
+            <StepButton color="inherit" onClick={handleStep(index)}>
+              {label}
+            </StepButton>
+          </Step>
+        ))}
+      </Stepper>
+
+      <div>
+        {allStepsCompleted() ? (
+          <React.Fragment>
+            <Typography sx={{ mt: 2, mb: 1 }}>
+              All steps completed - you&apos;re finished
+            </Typography>
+
+            <Button onClick={handleReset}>Reset</Button>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <Typography sx={{ mt: 2, mb: 1, py: 1 }}>
+              Step {activeStep + 1}
+            </Typography>
+            <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+              {completed[activeStep] ? (
+                <Typography variant="caption" sx={{ display: "inline-block" }}>
+                  Step {activeStep + 1} already completed
+                </Typography>
+              ) : (
+                <Button onClick={handleComplete}>Complete Step</Button>
+              )}
+            </Box>
+          </React.Fragment>
+        )}
+      </div>
+    </Box>
+  );
+}
